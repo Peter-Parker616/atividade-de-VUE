d613cb80848501529d76cf3affb5f22888ac2ff7
Peter-Parker616 <j040arthur555@gmail.com>
2025-09-29 19:51:02 -0300

    Create taskitem.vue

---
+++ taskitem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <span>{{ task.title }}</span>
+    <button @click="$emit('remove', task.id)">Remover</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0;
+  padding: 5px;
+  background: #f0f0f0;
+  border-radius: 4px;
+}
+
+button {
+  background: #ff4d4f;
+  color: white;
+  border: none;
+  padding: 2px 8px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
