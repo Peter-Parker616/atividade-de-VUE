51bc2a9539d431e971ecec8b2e5c79ca28780b7d
Peter-Parker616 <j040arthur555@gmail.com>
2025-09-29 19:49:45 -0300

    Create taskslist.vue

---
+++ taskslist.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <TaskItem 
+      v-for="task in tasks" 
+      :key="task.id" 
+      :task="task" 
+      @remove="removeTask"
+    />
+  </ul>
+</template>
+
+<script>
+import TaskItem from './TaskItem.vue'
+
+export default {
+  name: 'TaskList',
+  components: { TaskItem },
+  props: {
+    tasks: Array
+  },
+  methods: {
+    removeTask(taskId) {
+      this.$emit('remove-task', taskId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
